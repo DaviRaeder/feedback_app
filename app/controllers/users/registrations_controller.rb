6aaa59cef60ecc8f360def41dfb09d5f2abb1169
Juliana <Juliana@MacBook-Ju.local>
2024-11-18 10:00:15 -0300

    add a default value for role

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :role)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
   def account_update_params
